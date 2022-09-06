2d7919a8676ecf933ac7c977942e8200e0f2a532
Selcuk ARSLAN <slckarslan93@gmail.com>
2022-09-06 07:52:49 +0300

    Add project files.

---
+++ WebApi/Controllers/BookController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Entities;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class BookController : Controller
+    {
+        private static List<Book> BookList = new List<Book>()
+        {
+            new Book
+            {
+                Id = 1,
+                Title = "Lean Startup",
+                GenreId = 1, //Personal Growth
+                PageCount = 200,
+                PublishDate = new DateTime(2001,06,12),
+            },
+            new Book
+            {
+                Id=2,
+                Title="Herland",
+                GenreId = 2, //Science Fiction
+                PageCount=250,
+                PublishDate=new DateTime(2010,05,23)
+
+            },
+            new Book
+            {
+                Id=3,
+                Title="Dune",
+                GenreId = 2, //Science Fiction
+                PageCount=500,
+                PublishDate=new DateTime(2001,12,21)
+
+            }
+        };
+
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
+            return bookList;
+        }
+
+        [HttpGet("{id}")]
+        public Book GetById(int id)
+        {
+            var book = BookList.Where(x => x.Id == id).SingleOrDefault();
+            return book;
+        }
+
+        //FromQuery ile id den bu şekilde de çekebilriz ama BestPractice olan route ile getirmektir
+
+        //[HttpGet]  
+        //public Book Get([FromQuery] string id)
+        //{
+        //    var book = BookList.Where(x => x.Id == Convert.ToInt32(id)).SingleOrDefault();
+        //    return book;
+        //}
+    }
+}
