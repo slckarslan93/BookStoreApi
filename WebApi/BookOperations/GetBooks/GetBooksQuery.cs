8218f64f193b3a9e17771e2f54e5b9424335e947
Selcuk ARSLAN <slckarslan93@gmail.com>
2022-09-20 17:13:51 +0300

    commit

---
+++ WebApi/BookOperations/GetBooks/GetBooksQuery.cs
@@ -0,0 +1,42 @@
+﻿using WebApi.Common;
+using WebApi.DBOperations;
+using WebApi.Entities;
+
+namespace WebApi.BookOperations.GetBooks
+{
+    public class GetBooksQuery
+    {
+        private readonly BookStoreDbContext _dbContext;
+
+        public GetBooksQuery(BookStoreDbContext dbContext)
+        {
+            _dbContext = dbContext; 
+        }
+        public List<Book> Handle()
+        {
+            var bookList = _dbContext.Books.OrderBy(x => x.Id).ToList<Book>();
+            List<BookViewModel> vm = new List<BookViewModel>();
+            foreach (var book in bookList)
+            {
+                vm.Add(new BookViewModel()
+                {
+                   Title = book.Title,
+                   Genre=((GenreEnum)book.GenreId).ToString(),
+                   PublisDate=book.PublishDate.Date.ToString("dd/MM/yyy"),
+                   PageConunt=book.PageCount
+                });
+            }
+            return bookList;
+        }
+
+    }
+
+    public class BookViewModel
+    {
+        public string Title { get; set; }
+        public int PageConunt { get; set; }
+        public string PublisDate { get; set; }
+        public string Genre { get; set; }
+    }
+}
+
--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using WebApi.BookOperations.CreateBooks;
+using WebApi.BookOperations.GetBooks;
 using WebApi.DBOperations;
 using WebApi.Entities;
+using static WebApi.BookOperations.CreateBooks.CreateBookCommand;
 
 namespace WebApi.Controllers
 {
@@ -17,15 +20,11 @@ namespace WebApi.Controllers
         }
 
         [HttpGet]
-        public  List<Book> GetList()
+        public IActionResult GetBooks()
         {
-            var result = _context.Set<Book>().OrderBy(x=>x.Id).ToList();
-            return result;
-
-            //var bookList = _context.OrderBy(x => x.Id).ToList<Book>();
-            //return bookList;
-
-            
+            GetBooksQuery query = new GetBooksQuery(_context);
+            var result = query.Handle();
+            return Ok(result);
         }
 
         [HttpGet("{id}")]
@@ -33,10 +32,6 @@ namespace WebApi.Controllers
         {
             var book = _context.Set<Book>().Where(x => x.Id == id).SingleOrDefault();
             return book;
-
-
-            //var book = BookList.Where(x => x.Id == id).SingleOrDefault();
-            //return book;
         }
 
         //FromQuery ile id den bu şekilde de çekebilriz ama BestPractice olan route ile getirmektir
@@ -49,24 +44,28 @@ namespace WebApi.Controllers
         //}
 
         [HttpPost]
-        public IActionResult AddBook([FromBody] Book newBook)
+        public IActionResult AddBook([FromBody] CreateBookModel newBook)
         {
-            var book = _context.Set<Book>().SingleOrDefault(x => x.Title == newBook.Title);
-            if (book!= null)
+            CreateBookCommand command = new CreateBookCommand(_context);
+            try
             {
-                return BadRequest();
+                command.Model = newBook;
+                command.Handle();
             }
-            //BookList.Add(newBook);
-            _context.Add(newBook);
-            _context.SaveChanges();
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
             return Ok();
         }
+
         [HttpPut("{id}")]
         public IActionResult UpdateBook(int id, [FromBody] Book updatedbook)
         {
-            //var book = BookList.SingleOrDefault(x=>x.Id == id);
-            var book = _context.Set<Book>().SingleOrDefault(x=>x.Id==id);
-            if (book==null)
+            var book = _context.Set<Book>().SingleOrDefault(x => x.Id == id);
+            if (book == null)
             {
                 return BadRequest();
             }
@@ -75,17 +74,15 @@ namespace WebApi.Controllers
             book.PublishDate = updatedbook.PublishDate != default ? updatedbook.PublishDate : book.PublishDate;
             book.Title = updatedbook.Title != default ? updatedbook.Title : book.Title;
 
-            _context.Update(book);
+
             _context.SaveChanges();
             return Ok();
         }
         [HttpDelete("{id}")]
         public IActionResult DeleteBook(int id)
         {
-            //var book = BookList.SingleOrDefault(x=>x.Id == id);
-
             var book = _context.Set<Book>().SingleOrDefault(x => x.Id == id);
-            
+
             if (book == null)
             {
                 return BadRequest();
